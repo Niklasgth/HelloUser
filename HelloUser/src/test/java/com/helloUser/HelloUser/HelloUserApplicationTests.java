9ab01aefca36771bea0636c516c8494d3e6bd8e6
Niklas <Niklasgth@hotmail.com>
2024-10-28 13:52:01 +0100

    start

---
+++ HelloUser/src/test/java/com/helloUser/HelloUser/HelloUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloUser.HelloUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
