3d764d17d0315e1f5d48893a3fa3f9c94d081b34
Niklas <Niklasgth@hotmail.com>
2024-10-29 10:21:35 +0100

    commit

--- HelloUser/src/main/java/com/helloUser/HelloUser/FormController.java
+++ HelloUser/src/main/java/com/helloUser/HelloUser/FormController.java
@@ -1,16 +1,30 @@
 package com.helloUser.HelloUser;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class FormController {
     
     @GetMapping("/form")
-    public String getForm() {
+    public String getForm(Model model) {
+        model.addAttribute("member", new Member(null, 0, false, 0));
+        System.out.println(model+"Printar model");
         return "form";
     }
     
+    @PostMapping("/create-new-member")
+    public String newMember(Member member) {
+        System.out.println("Member added: " + member);
+        
+        return "redirect:/form";
+    }
 }
+
+
+
+
+
--- HelloUser/src/main/java/com/helloUser/HelloUser/MemberListController.java
+++ HelloUser/src/main/java/com/helloUser/HelloUser/MemberListController.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
+
+
+
 
 
 @Controller
@@ -21,14 +23,12 @@ public class MemberListController {
         members.add(new Member("Tyll", 33, false,4));
     }
 
-
-
-
 @GetMapping("/memberlist")
 public String getMemberList(Model model) {
     model.addAttribute("members", members);
     return "memberlist";
 }
+
 
 
 }
